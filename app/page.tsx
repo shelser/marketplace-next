9bfa800ced69f2fc8d7342b6e915ec43e9d47d27
shelser <shelserg@gmail.com>
2025-11-20 21:22:30 +0300

    added new files

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,161 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="container">
+      <div className="row">
+        <div className="col-3 col-xl-2 d-none d-lg-block">
+          <div className="filter">
+            <div className="filter-title">
+              <h5>Фильтр</h5>
+            </div>
+            <div className="filter-price">
+              <div className="filter-price_title">Цена</div>
+              <form>
+                <div className="filter-price_range">
+                  <div className="filter-price_input-wrapper">
+                    <label htmlFor="min" className="filter-price_label">
+                      от
+                    </label>
+                    <input id="min" className="filter-price_input" />
+                  </div>
+                  <div className="filter-price_input-wrapper">
+                    <label htmlFor="max" className="filter-price_label">
+                      до
+                    </label>
+                    <input id="max" className="filter-price_input" />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="filter-check">
+              <label className="filter-check_label">
+                <input
+                  type="checkbox"
+                  className="filter-check_checkbox"
+                  id="discount-checkbox"
+                />
+                <span className="filter-check_checkmark"></span>
+                <span className="filter-check_label-text">Акция</span>
+              </label>
+            </div>
+          </div>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="col-12 col-lg-9 col-xl-10">
+          <div className="container">
+            <div className="row no-gutters goods">
+              {/* <!-- КАРТОЧКИ ТОВАРОВ -->
+							<!-- <div className="col-12 col-md-6 col-lg-4 col-xl-3">
+								<div className="card">
+									<div className="card-img-wrapper">
+										<span className="card-img-top"
+											style="background-image: url('https://cdn1.ozone.ru/multimedia/c400/1033180284.jpg')"></span>
+									</div>
+									<div className="card-body justify-content-between">
+										<div className="card-price">33990 ₽</div>
+										<h5 className="card-title">Игровая приставка Sony PlayStation 4 Pro</h5>
+										<button className="btn btn-primary">В корзину</button>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-lg-4 col-xl-3">
+								<div className="card">
+									<div className="card-sale">🔥Hot Sale🔥</div>
+									<div className="card-img-wrapper">
+										<span className="card-img-top"
+											style="background-image: url('https://cdn1.ozone.ru/multimedia/c400/1027495663.jpg')"></span>
+									</div>
+									<div className="card-body justify-content-between">
+										<div className="card-price">16499 ₽</div>
+										<h5 className="card-title">Игровая приставка Sony PlayStation 3 Super Slim</h5>
+										<button className="btn btn-primary">В корзину</button>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-lg-4 col-xl-3">
+								<div className="card">
+									<div className="card-img-wrapper">
+										<span className="card-img-top"
+											style="background-image: url('https://cdn1.ozone.ru/multimedia/c400/1024358491.jpg')"></span>
+									</div>
+									<div className="card-body justify-content-between">
+										<div className="card-price">39990 ₽</div>
+										<h5 className="card-title">Игровая приставка Xbox One X</h5>
+										<button className="btn btn-primary">В корзину</button>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-lg-4 col-xl-3">
+								<div className="card">
+									<div className="card-sale">🔥Hot Sale🔥</div>
+									<div className="card-img-wrapper">
+										<span className="card-img-top"
+											style="background-image: url('https://cdn1.ozone.ru/multimedia/c400/1024822131.jpg')"></span>
+									</div>
+									<div className="card-body justify-content-between">
+										<div className="card-price">23411 ₽</div>
+										<h5 className="card-title">Игровая приставка Xbox One S</h5>
+										<button className="btn btn-primary">В корзину</button>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-lg-4 col-xl-3">
+								<div className="card">
+									<div className="card-img-wrapper">
+										<span className="card-img-top"
+											style="background-image: url('https://cdn1.ozone.ru/multimedia/c400/1021386685.jpg')"></span>
+									</div>
+									<div className="card-body justify-content-between">
+										<div className="card-price">24751 ₽</div>
+										<h5 className="card-title">Игровая приставка Nintendo Switch</h5>
+										<button className="btn btn-primary">В корзину</button>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-lg-4 col-xl-3">
+								<div className="card">
+									<div className="card-sale">🔥Hot Sale🔥</div>
+									<div className="card-img-wrapper">
+										<span className="card-img-top"
+											style="background-image: url('https://cdn1.ozone.ru/multimedia/c400/1024928305.jpg')"></span>
+									</div>
+									<div className="card-body justify-content-between">
+										<div className="card-price">3624 ₽</div>
+										<h5 className="card-title">Игровая приставка Sega Retro Genesis HD</h5>
+										<button className="btn btn-primary">В корзину</button>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-lg-4 col-xl-3">
+								<div className="card">
+									<div className="card-sale">🔥Hot Sale🔥</div>
+									<div className="card-img-wrapper">
+										<span className="card-img-top"
+											style="background-image: url('https://cdn1.ozone.ru/multimedia/c400/1021877092.jpg')"></span>
+									</div>
+									<div className="card-body justify-content-between">
+										<div className="card-price">1551 ₽</div>
+										<h5 className="card-title">Игровая приставка Dendy Junior</h5>
+										<button className="btn btn-primary">В корзину</button>
+									</div>
+								</div>
+							</div>
+							<div className="col-12 col-md-6 col-lg-4 col-xl-3">
+								<div className="card">
+									<div className="card-img-wrapper">
+										<span className="card-img-top"
+											style="background-image: url('https://cdn1.ozone.ru/multimedia/c400/1025222877.jpg')"></span>
+									</div>
+									<div className="card-body justify-content-between">
+										<div className="card-price">10445 ₽</div>
+										<h5 className="card-title">Игровая приставка Sony PlayStation classNameic</h5>
+										<button className="btn btn-primary">В корзину</button>
+									</div>
+								</div>
+							</div> -->
+							<!-- END КАРТОЧКИ ТОВАРОВ --> */}
+            </div>
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
---
+++ app/ui/Cart.tsx
@@ -0,0 +1,16 @@
+export default function Cart() {
+  return (
+    <div className="cart">
+      <div className="cart-body">
+        <div className="cart-title">Корзина</div>
+        <div className="cart-total">
+          Общая сумма: <span>0</span> руб
+        </div>
+
+        <div className="cart-wrapper"></div>
+        <button className="btn btn-primary cart-confirm">Оформить заказ</button>
+        <div className="cart-close"></div>
+      </div>
+    </div>
+  );
+}
